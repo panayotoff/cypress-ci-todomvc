3aa6e5b0fcfdffb91df53f9b705e4e96357d50b1
Chris Panayotoff <burnandbass@gmail.com>
2021-07-13 10:33:18 +0300

    Add failing test to see workflow

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -59,7 +59,8 @@ describe('TodoMVC', function () {
     .type('learn testing{enter}')
     .type('be cool{enter}')
 
-    cy.get('.todo-list li').should('have.length', 2)
+    // cy.get('.todo-list li').should('have.length', 2)
+    cy.get('.todo-list li').should('have.length', 3)
   })
 
   context('No Todos', function () {
